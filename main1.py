0048f0e2595dd0bd01a10678d7a53745bce8ea40
umme-682 <umme.salama144@gmail.com>
2024-12-31 14:04:34 +0500

    main1

---
+++ main1.py
@@ -0,0 +1,13 @@
+# program to check if the Nth bit is set or not
+
+def setOrNot(number, n):
+    
+    # make a mask variable by left shifting 1 (k-1) times and check if (n AND mask) equals 1 or 0
+    if number & (1 << (n - 1)):
+        print("\nSET")
+    else:
+        print("\nNOT SET")
+        
+number = int(input("Enter number: "))
+n = int(input("Enter Bit number: "))
+setOrNot(number, n)
